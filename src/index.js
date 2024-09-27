bc2be750899fad254e69ff145ea1ce440a46bb88
pracitce <109186405+pracitce@users.noreply.github.com>
2024-09-26 20:42:52 -0400

    updates css js html had problems but fix it.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,28 @@
- 
- //losAngeles 
- let losAngelesElement =document.querySelector("#los-angeles");
- let losAngelesDateElement = losAngelesElement.querySelector(".date");
- let losAngelesTimeElement = losAngelesTimeElement.querySelector(".time");
- 
- losAngelesDateElement.innerHTML = moment().format('dddd');
- losAngelesTimeElement.innerHTML = "1:45:15<small>am</small>";
+function updateTime() {
+
+    // Los Angeles
+    let losAngelesElement = document.querySelector("#los-angeles");
+    let losAngelesDateElement = losAngelesElement.querySelector(".date");
+    let losAngelesTimeElement = losAngelesElement.querySelector(".time");
+    let losAngelesTime = moment().tz("America/Los_Angeles");
+  
+    losAngelesDateElement.innerHTML = losAngelesTime.format("MMMM Do YYYY");
+    losAngelesTimeElement.innerHTML = losAngelesTime.format("h:mm:ss [<small>]A[</small>]");
+  
+    // Paris
+    let parisElement = document.querySelector("#paris");
+    let parisDateElement = parisElement.querySelector(".date");
+    let parisTimeElement = parisElement.querySelector(".time");
+    let parisTime = moment().tz("Europe/Paris");
+  
+    parisDateElement.innerHTML = parisTime.format("MMMM Do YYYY");
+    parisTimeElement.innerHTML = parisTime.format("h:mm:ss [<small>]A[</small>]");
+  
+  }
+  
+  // Call the function initially
+  updateTime();
+  
+  // Set the interval correctly
+  setInterval(updateTime, 1000);
+  
